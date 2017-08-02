ca7a676464c17c9a4a6857fb722e779b8ef3115d
harttle <yangjvn@126.com>
2017-08-02 16:29:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+This repo contains several packages to demonstrate apmjs usage.
+
+## Source Tree
+
+```
+```
---
+++ demo-hello/index.js
@@ -0,0 +1,5 @@
+define(function(){
+    var name = 'demo-hello'
+    console.log(name, 'loaded')
+    return name
+})
